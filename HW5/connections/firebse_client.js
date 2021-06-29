e6b291430b6e3227f4bbba33e66ff79c152143c7
曹邦煒 <u06410062@ccu.edu.tw>
2021-06-29 17:23:57 +0800

    update

---
+++ HW5/connections/firebse_client.js
@@ -0,0 +1,12 @@
+const firebase = require('firebase');
+require('dotenv').config();
+ 
+const config = {
+ apiKey: process.env.FIREBASE_API_KEY,
+ authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+ databaseURL: process.env.FIREBASE_DATABASEURL,
+ projectId: process.env.FIREBASE_PROJECT_ID,
+};
+ 
+firebase.initializeApp(config);
+module.exports = firebase;
